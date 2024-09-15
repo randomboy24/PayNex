3906a433b2f04344f4e52f85feddf83b4a3e8c5d
randomboy24 <jatinthegod21@gmail.com>
2024-09-15 19:17:59 +0530

    init

---
+++ apps/user-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home () {
+  return(
+    <div className="flex flex-col h-screen justify-center items-center text-green-700">
+      <div>hello </div>
+      <div>hheello</div>
+      <div>jewf</div>
+      <div>egar</div>
+    </div>
+  )
+}
